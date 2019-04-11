730e247e9f29e5817da9286f6adec71441f6f966
Sential <sentialx@gmail.com>
2019-04-11 14:42:34 +0200

    small update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-const { getIcon } = require("bindings")("addon");
+import { platform } from "os";
+
+let getIcon: any;
+
+if (platform() === 'win32') {
+  getIcon = require("bindings")("addon");
+}
 
 const getFileIcon = (path: string): Buffer => {
+  if (!getIcon) return;
   return getIcon(path);
 };
 
