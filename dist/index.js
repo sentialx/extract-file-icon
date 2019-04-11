6e91cde05f22084350a3d2c72ff46240c600fbff
Sential <sentialx@gmail.com>
2019-04-11 14:48:31 +0200

    small update

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { getIcon } = require("bindings")("addon");
+const os_1 = require("os");
+let getIcon;
+if (os_1.platform() === 'win32') {
+    getIcon = require("bindings")("addon");
+}
 const getFileIcon = (path) => {
+    if (!getIcon)
+        return;
     return getIcon(path);
 };
 exports.getFileIcon = getFileIcon;
